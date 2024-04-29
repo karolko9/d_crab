fc636963f50263adc31959788bb9ff9bb1812c18
Bolek121 <115473541+Bolek121@users.noreply.github.com>
2024-04-30 00:00:00 +0200

    front internet identity działa dobrze, backend ii działa na odwrót - jak zalogowany to error jak wylogowany to funkcja działa, naprawić backend, dlaczego cały czas mnie przekierowuje na oficjalną autoryzację, dodać teraz feautery które będą działać jedynie gdy zalogowany, zaplanować d_crab

--- src/v5_frontend/src/App.jsx
+++ src/v5_frontend/src/App.jsx
@@ -1,8 +1,28 @@
-import { useState } from 'react';
+import React, { useEffect, useCallback, useState } from "react";
 import { v5_backend } from 'declarations/v5_backend';
+import { isAuthenticated, getPrincipalText, login, logout} from "./utils/auth";
+
 
 function App() {
+  const [authenticated, setAuthenticated] = useState(false);
   const [greeting, setGreeting] = useState('');
+  const [principal, setPrincipal] = useState('');
+
+
+
+  useEffect(async () => {
+    const principal = await getPrincipalText();
+    setPrincipal(principal);
+  }, [setPrincipal]);
+  
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      setAuthenticated(await isAuthenticated());
+    };
+    checkAuthentication();
+  }, [setAuthenticated]);
+  
+  
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -15,15 +35,31 @@ function App() {
 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+      {authenticated ? (
+        <div>          
+          <div>
+            <div>authenticated={authenticated.toString()}</div>
+
+            <div>principal={principal}</div>
+            <button onClick={logout}>Log out</button>
+          
+          </div>
+          <div>
+            <form action="#" onSubmit={handleSubmit}>
+              <label htmlFor="name">Enter your name: &nbsp;</label>
+              <input id="name" alt="Name" type="text" />
+              <button type="submit">Click Me!</button>
+            </form>
+            <section id="greeting">{greeting}</section>
+          </div>
+        </div>
+      ) : (
+        <div>      
+          <div>not authenticated</div>
+          <button onClick={login}>Log in</button>
+        </div>
+      )}
+
     </main>
   );
 }
---
+++ src/v5_frontend/src/utils/auth.jsx
@@ -0,0 +1,67 @@
+import { AuthClient } from "@dfinity/auth-client";
+
+
+const IDENTITY_PROVIDER = `http://${process.env.IDENTITY_CANISTER_ID}.${window.location.hostname}:4943`;
+
+process.env.II_URL =
+    process.env.DFX_NETWORK === "local"
+        ? `http://${process.env.INTERNET_IDENTITY_CANISTER_ID}.localhost:4943/`
+        : `https://identity.ic0.app`;
+
+const MAX_TTL = 7 * 24 * 60 * 60 * 1000 * 1000 * 1000;
+
+export async function getAuthClient() {
+    return await AuthClient.create();
+}
+
+export async function getPrincipal() {
+    const authClient = await getAuthClient();
+    return authClient.getIdentity()?.getPrincipal();
+}
+
+export async function getPrincipalText() {
+    return (await getPrincipal()).toText();
+}
+
+export async function isAuthenticated() {
+    try {
+        const authClient = await getAuthClient();
+        return await authClient.isAuthenticated();
+    } catch (err) {
+        logout();
+    }
+}
+
+export async function login() {
+    console.log("Attempting login...");
+    console.log("Identity Provider URL:", process.env.II_URL); // Add this console log
+
+    try {
+        const authClient = await getAuthClient();
+        const isAuthenticated = await authClient.isAuthenticated();
+        
+        if (!isAuthenticated) {
+            console.log("User is not authenticated, initiating login...");
+            await authClient?.login({
+                identityProvider: process.env.II_URL,
+                onSuccess: async () => {
+                    console.log("Login successful, reloading page...");
+                    window.location.reload();
+                },
+                maxTimeToLive: MAX_TTL,
+            });
+        } else {
+            console.log("User is already authenticated, no need to login.");
+        }
+    } catch (error) {
+        console.error("Error occurred during login:", error);
+    }
+}
+
+
+
+export async function logout() {
+    const authClient = await getAuthClient();
+    authClient.logout();
+    window.location.reload();
+}
